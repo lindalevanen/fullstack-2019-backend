a469488de36149c40175e714f1497293eb10f7f4
Levänen Linda <levanel2@cs.hut.fi>
2019-11-19 17:33:20 +0200

    Puhelinluettelo backend step1 valmis

---
+++ puhelinluettelo-backend/index.js
@@ -0,0 +1,80 @@
+const express = require('express')
+const app = express()
+
+let persons = [
+  {
+    id: 1,
+    name: "Arto Hallas",
+    number: "040-2345665",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "050-3456414",
+  },
+  {
+    id: 3,
+    name: "Matti Meikäläinen",
+    number: "040-3406905",
+  }
+]
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
+})
+
+const generateId = () => {
+  const maxId = persons.length > 0
+    ? Math.max(...persons.map(n => n.id))
+    : 0
+  return maxId + 1
+}
+
+
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.content) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
+})
+
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
