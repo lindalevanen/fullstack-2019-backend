4cec85ef1d184c79b5d8e3929ed502737dcb6a7a
Levänen Linda <levanel2@cs.hut.fi>
2019-11-27 12:45:39 +0200

    4.4 done

--- blogilista/utils/list_helper.js
+++ blogilista/utils/list_helper.js
@@ -2,6 +2,13 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  return blogs.reduce((likeSum, current) => {
+    return likeSum + current.likes
+  }, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
