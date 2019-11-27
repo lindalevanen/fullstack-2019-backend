3936ec1108021c7a6afb34bf9c5c01c711c240ba
Levänen Linda <levanel2@cs.hut.fi>
2019-11-27 11:53:17 +0200

    4.2 done

---
+++ blogilista/utils/middleware.js
@@ -0,0 +1,29 @@
+const morgan = require('morgan')
+
+morgan.token('body', function(req) {
+  return JSON.stringify(req.body)
+})
+
+const requestLogger = morgan(':method :url :status :res[content-length] - :response-time ms :body')
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: 'unknown endpoint' })
+}
+
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError' && error.kind == 'ObjectId') { // eslint-disable-line
+    return res.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return res.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
+
+module.exports = {
+  requestLogger,
+  unknownEndpoint,
+  errorHandler
+}
